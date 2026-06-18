d767ede0e23c5441f3a15f2066ec839777298d11
Udayan-Sparkout <udayan@sparkouttech.com>
2026-06-18 21:25:30 +0530

    old commit

--- learn.js
+++ learn.js
@@ -4,3 +4,6 @@ setTimeout(function a(){
 
 },5000
 )
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+.then(response => response.json())
+.then(json => {console.log(json)}).catch(error => console.log(error))
